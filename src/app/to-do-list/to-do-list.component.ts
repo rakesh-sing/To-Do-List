1596b7c89e0140910f0a1e5e8b9b200f2582bb6f
Rakesh Sing <rakesh-sing@outlook.com>
2025-04-13 21:32:42 +0530

    template

---
+++ src/app/to-do-list/to-do-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list',
+  standalone: false,
+  templateUrl: './to-do-list.component.html',
+  styleUrl: './to-do-list.component.css'
+})
+export class ToDoListComponent {
+
+}
