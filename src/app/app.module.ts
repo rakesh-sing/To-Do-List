2116a42f4fa4fc6a1fe6a96b06858b1b18ce99a9
Rakesh Sing <rakesh-sing@outlook.com>
2025-04-14 00:23:04 +0530

    to do

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ToDoListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ToDoListComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do-list',
   standalone: false,
   templateUrl: './to-do-list.component.html',
-  styleUrl: './to-do-list.component.css'
+  styleUrl: './to-do-list.component.css',
 })
-export class ToDoListComponent {
+export class ToDoListComponent implements OnInit {
+  taskArray = [{ taskName: 'Brush teeth', isCompleted: false }];
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false,
+    });
+
+    form.reset();
+  }
+
+  onDelete(index: number) {
+    console.log(index);
+
+    this.taskArray.splice(index, 1);
+  }
+
+  onCheck(index: number) {
+    console.log(this.taskArray);
+
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
 }
